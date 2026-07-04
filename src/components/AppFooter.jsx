e112c3073401b425896b369df38df8a748b832f3
kadjivdev <kadjivdev@gmail.com>
2026-07-04 09:54:24 +0100

    revu du footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,7 +3,7 @@ import { CFooter } from '@coreui/react'
 
 const AppFooter = () => {
   return (
-    <CFooter className="fixed-bottom mt-5">
+    <CFooter className="mt-5">
       <div className="w-100 text-center">
         <span className="ms-1">&copy; Copyright {new Date().getFullYear()} | <strong className="text-yellow">Kadjiv'Sarl</strong> | Tous droit réservés.</span>
       </div>
